ebe54b9148a4520f3ec8b98539e654bd1954a25a
ShlokModi <shlokmodi2008@gmail.com>
2020-06-20 13:03:12 +0530

    asynchronousBallMovement

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var ball;
-
+var database,position;
 function setup(){
+    database = firebase.database();
+    var ballPosition = database.ref("Ball/Position");
+    ballPosition.on("value",readPosition,showError);
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
@@ -8,17 +11,19 @@ function setup(){
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
     }
     drawSprites();
 }
@@ -26,4 +31,18 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError(){
+    console.log("error");
+}
+function writePosition(x,y){
+    database.ref("Ball/Position").set({
+        x: position.x + x,
+        y: position.y + y
+    })
 }
